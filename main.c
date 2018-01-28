d7619dbd3c09d7f210caf4d733b35e7c0c97210b
Marcel Müller <muellerphysics@gmail.com>
2018-01-28 16:55:55 +0100

    SIM AVR added to test timing

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
-
 #include <stdint.h>
+#include <avr/io.h>
+#include <avr/sleep.h>
 
-uint8_t command_list[8192];
+// TODO: enclose in ifdef for simulator compule?
+#include <avr_mcu_section.h>
+AVR_MCU(F_CPU,"atmega88");
 
+const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
+    { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, },
+    //{ AVR_MCU_VCD_SYMBOL("UDRE0"), .mask = (1 << UDRE0), .what= (void*)&UCSR0A, },
+};
+
+
+uint8_t timing_table[8192] = {0x11 };
 
 int main() {
 
-    
+    DDRB=0xff;
 
+    for (;;) {
+
+    PORTB = 0x01;
+    for (uint16_t i=0; i<1024; i++) {
+	__asm__("nop");
+    }
+    PORTB = 0x00;
+    
+    for (uint16_t i=0; i<1024; i++) {
+	__asm__("nop");
+    }
+    }
+    sleep_cpu();
 }
