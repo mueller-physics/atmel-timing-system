88f89068d317cdc678bd9793c2104754cf172e17
Marcel Müller <muellerphysics@gmail.com>
2018-01-29 01:16:26 +0100

    simavr working, interpreter starts working
    State and ideas:
    
    * Can turn pins on and off from interpreted table.
    * Currently 24 cycles (1.5us on an Arduino), either bring this down to 16 (1us), or up to 32 (2us)
    * Jump table instead of branch instructions, will make it faster and reliable
    * Encode port into command

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@
 
 all:	main.c interpreter.S
 	avr-gcc -Wall -gdwarf-2 -Os -std=gnu99 \
-			-mmcu=atmega48 \
-			-DF_CPU=16000000 \
+			-mmcu=atmega328 \
+			-DF_CPU=1000000 \
 			-fno-inline-small-functions \
 			-ffunction-sections -fdata-sections \
 			-Wl,--relax,--gc-sections \
 			-Wl,--undefined=_mmcu,--section-start=.mmcu=0x910000 \
 			-I/home/marcel/tmp/simavr/simavr/sim/avr  \
-			main.c interpreter.S -o test.axf
+			main.c interpreter.S asmtest.S -o test.axf
--- main.c
+++ main.c
@@ -1,34 +1,113 @@
-#include <stdint.h>
 #include <avr/io.h>
+#include <stdio.h>
+#include <stdint.h>
 #include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 // TODO: enclose in ifdef for simulator compule?
 #include <avr_mcu_section.h>
-AVR_MCU(F_CPU,"atmega88");
+AVR_MCU(F_CPU,"atmega328");
+AVR_MCU_VCD_FILE("gtkwavetrace.vcd", 1);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, },
-    //{ AVR_MCU_VCD_SYMBOL("UDRE0"), .mask = (1 << UDRE0), .what= (void*)&UCSR0A, },
+    { AVR_MCU_VCD_SYMBOL("PORTB1"), .mask = (2 ), .what= (void*)&PORTB, },
 };
 
 
-uint8_t timing_table[8192] = {0x11 };
+static int uart_putchar(char c, FILE *stream) {
+  if (c == '\n')
+    uart_putchar('\r', stream);
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = c;
+  return 0;
+}
+
+
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,
+                                         _FDEV_SETUP_WRITE);
+
+
+extern void timing_interpreter(); 
+extern void asmtest();
+
+uint8_t spacing[80] ={0,1,2,3,4,5,6};
+
+
+volatile uint8_t timing_table[90] = {
+
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x11,   // set low on port b
+    0xFF,   // all pins
+    0x21,   // set high on port b
+    0xFF,   // all pins  
+    0x70    // end program 
+}; 
+
 
 int main() {
 
     DDRB=0xff;
+   
+    stdout= &mystdout;
+    /*
+    stdout = &mystdout;
 
-    for (;;) {
 
-    PORTB = 0x01;
-    for (uint16_t i=0; i<1024; i++) {
-	__asm__("nop");
-    }
-    PORTB = 0x00;
+    printf("timing_table %d (%d,%d) \n",
+	(int)&timing_table, (int)((&timing_table))&0xFF, (int)(&timing_table)>>8);
+
+    */
     
-    for (uint16_t i=0; i<1024; i++) {
-	__asm__("nop");
-    }
-    }
+    cli();
+    timing_interpreter();
+    
+    //asmtest();
+    cli();
+
+
     sleep_cpu();
 }
